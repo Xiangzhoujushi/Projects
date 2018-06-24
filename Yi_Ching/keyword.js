f53eb84657941ffff69fb2a735c2c19848f6aec5
Xiangzhoujushi <tang.794@osu.edu>
2018-06-24 15:10:56 -0400

    add

---
+++ Yi_Ching/keyword.js
@@ -0,0 +1,461 @@
+var sentimentsObject = [
+	// 1 是 positive
+	// 2 是 neutral
+// 3 是偏向 negative
+{'name':'positive','arr':['利','吉','貞吉','元','亨','悔亡','利貞','宜','貞','顒','尚','孚','得']},
+{'name':'neutral','arr':['无咎','勿憂','无疾','无喪','无得']},
+{'name':'negative','arr':['不利','凶','眚','匪正','孚窒','不寧','用獄','不家食','厲','喪','不宜','勿用']}
+];
+
+var translation = {
+	"advantage": "利",
+	"auspicious":"吉",
+	"pure fortune":"貞吉",
+	"primary fortune": "元", 
+	"prosperous":"亨",
+	'advantageous':'利',
+	"regret vanishes":'悔亡',
+	"abundant and pure":'利貞',
+	"appropriate":'宜',
+	"confident":'有孚',
+	"determination":'貞',
+	"reverence":'顒',
+	"honored":'尚',
+	"sincerity":'孚',
+	"gain":'得',
+	"no misfortune":'无咎',
+	"no worries":'勿憂',
+	"no fault":'无疾',
+	"no loss":'无喪',
+	"no gain":'无得',
+	"have misfortune":'貞凶',
+	"disadvantage":'不利',
+	"ominous":'凶',
+	"troublesome":'眚',
+	"untruthful":'匪正',
+	"lose faith":"孚窒",
+	"lost friends":'喪朋',
+	"restless":'不寧',
+	"prosecuting":'用獄',
+	"not eating at home":'不家食',
+	"threatening":'厲',
+	"loss":'喪',
+	"not appropriate":'不宜',
+	"no engaging":'勿用'
+}
+
+
+var sentimentTable = {
+	'positive':0,
+	'neutral':1,
+	'negative':2
+};
+
+var sentimentClass = ['Positive','Neutral','Negative']
+// array for labelling the colored or uncolored rectangles
+// var labelArr = new Array(numberOfLayer1)
+// the color list for yin and yang
+var colorList = ['#0cccd6','#ff3e13'];
+var sentimentsCount = {};
+// for (key in translation.keys){
+// 	value = translation[key]
+// 	sentimentsCount[key] = [0,value,]
+
+// }	
+// sort sentiment by length of the string
+sentimentsArray = []
+sentimentsObject.forEach(
+	function(d){
+		var array = d.arr
+		var name = d.name
+		array.forEach(
+			function(d2){
+				sentimentsArray.push({'id':d2,'label':name})
+				key = findTranslation(d2)
+				sentimentsCount[key] = [d2,name,0]
+			}
+		)
+	}
+)
+
+function findTranslation(characters){
+				for (key in translation){
+					value = translation[key];
+					if (value == characters){
+						return key
+					}
+
+				}
+
+}
+		// sort the sentiments array by lenghth
+sentimentsArray.sort(
+	function(a,b){
+		return b['id'].length-a['id'].length
+	}
+)	
+
+
+// the word cloud charts using high chart API, alternative way 
+function wordCloudHighChart(){
+	var bgc ="#262626"
+	var maxFontSize = 30
+	var chartHeight = 270
+	var chartWidth = 350
+	var bgc ="#262626"
+
+	var chartArgs = {
+		backgroundColor:bgc,
+		// height:chartHeight,
+		width:chartWidth,
+		marginLeft:0,
+		marginRight:0,
+		marginBottom:0,
+		marginTop:20
+	}
+
+	// var chartTitile = {
+	var titleStyle = {color: "white",
+					'font-size':'15px'
+	}
+	console.log(AllData);
+
+
+	var getRandomPosition = function getRandomPosition(size) {
+	  return Math.round(size * Math.random());
+	};
+
+	var randomPlacement = function randomPlacement(point, options) {
+	  var field = options.field,
+	    r = options.rotation;
+	    console.log(field.width);
+	  return {
+	    x: getRandomPosition(field.width) - (field.width*2 /3)-10,
+	    y: getRandomPosition(field.height*2) - (field.height*2 / 3),
+	    rotation: 0
+	  };
+	}
+
+	Highcharts.seriesTypes.wordcloud.prototype.placementStrategy.randomHorizontal = randomPlacement;
+	// }
+	//All groups: positive, neutral, and negative
+	Highcharts.chart('Positive', {
+		maxFontSize: 30,
+		// minFontSize:15,
+		plotOptions:{
+			series: {
+				point: {
+					events: {
+					mouseOver: function () {
+					var chart = this.series.chart;
+					var chineseName = this.chinese;
+					highLightKeyWord(chineseName);
+					// console.log(chineseName)
+					
+					},
+					mouseOut: function () {
+						unhighLightKeyWord()
+					}
+				}	
+				}
+			}
+		},
+		chart:chartArgs,
+		series: [{
+			type: 'wordcloud',
+			data: positiveData,
+			name: 'Occurrences in Guaci',
+			placementStrategy: 'randomHorizontal'
+		}],
+		title: {
+			text: 'Positive Key Words',
+			style: titleStyle,
+			align:'right',
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			pointFormat: '<span >{point.name}</span>: <b>{point.count}</b> of total<br/>'
+		},
+		credits:{
+			enabled: false
+		},
+		colors:[colorForGua[0]]
+	});
+
+
+	// neutral Key Words
+	Highcharts.chart('Neutral', {
+		maxFontSize: 30,
+		// minFontSize:15,
+		plotOptions:{
+			series: {
+				point: {
+					events: {
+					mouseOver: function () {
+					var chart = this.series.chart;
+					var chineseName = this.chinese;
+					highLightKeyWord(chineseName);
+					// console.log(chineseName)
+					
+					},
+					mouseOut: function () {
+						unhighLightKeyWord()
+					}
+				}	
+				}
+			}
+		},
+		//
+		chart:chartArgs,
+		
+		series: [{
+			type: 'wordcloud',
+			data: neutralData,
+			name: 'Occurrences in Guaci'
+		}],
+		tooltip: {
+			headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			pointFormat: '<span >{point.name}</span>: <b>{point.count}</b> of total<br/>'
+		},
+		title: {
+			text: 'Neutral Key Words',
+			style: titleStyle,
+			align:'right'
+		},
+		credits:{
+			enabled: false
+		},
+		colors:[colorForGua[1]]
+	});
+
+	//negative Key Words
+	Highcharts.chart('Negative', {
+		maxFontSize: 30,
+		// minFontSize:15,
+		plotOptions:{
+			series: {
+				point: {
+					events: {
+					mouseOver: function () {
+					var chart = this.series.chart;
+					var chineseName = this.chinese;
+					highLightKeyWord(chineseName);
+					// console.log(chineseName)						
+					},
+					mouseOut: function () {
+						unhighLightKeyWord()
+					}
+				}	
+				}
+			}
+		},
+		//
+		chart:chartArgs,
+		series: [{
+			type: 'wordcloud',
+			data: negativeData,
+			name: 'Occurrences in Guaci'
+		}],
+		title: {
+			text: 'Negative Key Words',
+			style: titleStyle,
+			align:'right'
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			pointFormat: '<span >{point.name}</span>: <b>{point.count}</b> of total<br/>'
+		},
+		credits:{
+			enabled: false
+		},
+		colors: [colorForGua[2]]
+	});
+	//Generate the tag clouds
+	//click function call will render the highlight of the guaci's colors
+
+}
+
+// highlight the key word
+function highLightKeyWord(word){
+	// go over the hexagrams and color the rectangle
+	var svg = d3.select('#BieGua>svg')
+	var newGraph = svg.append('g')
+	.attr("transform",
+		function(){
+			var result = "translate("+200+","+200+")";
+			return result;
+		}
+	)
+	.attr('class','highlight');
+	var squareLength = 7;
+	// the interval between each rectangel
+	var interval = 1;
+	// the number of yao in each layer
+	var guaLayer = 3;
+	// the width of the layer
+	var layerWidth = 7;
+
+	var centerX = 200;
+	var centerY = 200;
+	hexagrams.forEach(function(data,iprime){
+		ci = guaci[iprime]
+		var index = ci.indexOf(word)
+		// console.log(ci)
+		if(index>-1 && ci[index-1]!="不" && ci[index-1]!="无"){
+			// console.log("enter")
+			var angle = 360/64.0*iprime;
+			var subgraph = newGraph.append('g');
+			// rotate through the graph
+			subgraph.attr("transform",
+				function(){
+					var result = "rotate("+(1*angle)+","+centerX+","+centerY+") ";
+					result+="translate("+400+","+200+")"
+					return result;
+				}
+			);
+			var numberOfLayer1 = getNumOfLayer(iprime);
+			var startX =guaLayer*squareLength+guaLayer*interval;
+			var startY = -1;
+			var endY = -1;
+			var endX = startX+numberOfLayer1*layerWidth;
+			subgraph.append('rect')
+				.attr('x',startX+layerWidth*index+0.5)
+				.attr('y',-0.5)
+				.attr('width',function(){
+					// return squareLength;
+					return layerWidth * word.length-1;
+				})
+				.attr('height',function(){
+					// return squareLength;
+					return 2*layerWidth+interval+1;
+				})
+				.style('fill', function(){
+					// highlight using white
+					return "white";
+			});
+
+		}
+
+	})
+}
+
+// unhighlight the keyword
+function unhighLightKeyWord(){
+	d3.select('.highlight').remove()
+}
+
+// render the word clouds
+function renderTagCloudsForSentiments(){
+	positiveData =[];
+	neutralData =[];
+	negativeData =[];	
+	for (var key in sentimentsCount){
+		// console.log(key)
+		if (sentimentsCount[key][1] == "positive"){
+			positiveData.push(
+				{"name":key+"("+sentimentsCount[key][0]+")",
+				"chinese":sentimentsCount[key][0],
+				"count":sentimentsCount[key][2],
+				'tag':0,
+				"color":colorForGua[0],
+				"weight":normalizeCount(sentimentsCount[key][2])}
+				);
+		}
+		if (sentimentsCount[key][1] == "neutral"){
+			neutralData.push(
+				{"name":key+"("+sentimentsCount[key][0]+")",
+				"chinese":sentimentsCount[key][0],
+				"count":sentimentsCount[key][2],
+				'tag':1,
+				"color":colorForGua[1],
+				"weight":normalizeCount(sentimentsCount[key][2])}
+				);
+		}
+		if (sentimentsCount[key][1] == "negative"){
+			negativeData.push(
+				{"name":key+"("+sentimentsCount[key][0]+")",
+				"chinese":sentimentsCount[key][0],
+				"count":sentimentsCount[key][2],
+				'tag':2,
+				"color":colorForGua[2],
+				"weight":normalizeCount(sentimentsCount[key][2])}
+				);
+		}
+
+	}
+	// console.log(positiveData)
+	//A link to the mouseover, mouse on events
+	AllData = positiveData.concat(neutralData,negativeData)
+	//http://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/highcharts/plotoptions/series-point-events-mouseover/
+	var fontSize = 14;
+	var width = 400;
+	var background = '#262626';
+	var height = 900;
+	var space  = 8;
+	var margin = 30;
+	var marginLeft = 50;
+	var factor = 0;
+	// //Tag clouds For all chinese characters
+	var svg = d3.select('#MainText').append('svg').style("background-color", background)
+	.attr('height',height).attr('width',width)
+	.style('opacity',0.95)
+	;
+	// get the counts for each elements in positive group
+	// var svg = d3.select()
+	// space between words
+	
+	var textGraph = svg.append('g').attr('transform',
+		'translate(50,20)'
+	)
+	var wordClouds = textGraph.selectAll('text')
+	.data(AllData).enter().append('text')
+	.text(function(d){
+		return d.name
+	})
+	.attr('x', d =>  {
+		x = Math.random()*(width*0.2)+100;
+		return x;
+	})
+	.attr('y', (d,i)=>{
+		// console.log(AllData[i-1])
+		// var prevData = 
+		if (i<=0 || (AllData[i-1].tag!= d.tag)){
+			// labels for classification of keywords 
+			factor += 25;
+			textGraph.append('rect')
+			.attr('x',width-width/2)
+			.attr('y',i*(fontSize+space)+factor-30)
+			.attr('height',15)
+			.attr('width',width/2)
+			.style('fill',d.color);
+
+			textGraph.append('text')
+			.attr('x',width-width/2)
+			.attr('y',i*(fontSize+space)+factor-35/2)
+			.style('fill','white')
+			.style('fill-opacity',0.8)
+			.text(sentimentClass[d.tag])
+			.style('font-size', fontSize-2)
+			factor += 4;
+			// .attr('height',15)
+			// .attr('width',width/2)
+			// .style('fill',d.color);
+			// append the title
+		}
+		var  yPosition = i*(fontSize+space)+factor
+		// yPosition+=factor;
+		return yPosition;
+	})
+	.style('fill',d=> {return d.color})
+	.style('font-size', fontSize)
+	.on('mouseover',function(d){
+	var name = d.chinese;
+	highLightKeyWord(name)
+	// console.log(d)
+	}).on('mouseout',function(d){
+		unhighLightKeyWord();
+	});
+	// append the title for the key words
+				// append the 
+}
+
---
+++ Yi_Ching/themes.js
@@ -0,0 +1,442 @@
+var themes = {
+	name: "theme", value:91,
+	children:[
+		{name:"Mindset",value:20,
+		children:[
+			{name:2,value:1},
+			{name:3,value:1},
+			{name:4,value:1},
+			{name:5,value:1},
+			{name:6,value:1},
+			{name:34,value:1},
+			{name:9,value:1},
+			{name:10,value:1},
+			{name:11,value:1},
+			{name:12,value:1},
+			{name:15,value:1},
+			{name:61,value:1},
+			{name:17,value:1},
+			{name:41,value:1},
+			{name:51,value:1},
+			{name:25,value:1},
+			{name:29,value:1},
+			{name:30,value:1},
+			{name:63,value:1},
+			{name:64,value:1}
+		]	
+		},
+			{name:"Behavior",value:38,
+			children:[
+				{name:4,value:1},
+				{name:6,value:1},
+				{name:7,value:1},
+				{name:10,value:1},
+				{name:11,value:1},
+				{name:12,value:1},
+				{name:20,value:1},
+				{name:21,value:1},
+				{name:22,value:1},
+				{name:23,value:1},
+				{name:24,value:1},
+				{name:25,value:1},
+				{name:26,value:1},
+				{name:27,value:1},
+				{name:28,value:1},
+				{name:29,value:1},
+				{name:30,value:1},
+				{name:31,value:1},
+				{name:32,value:1},
+				{name:33,value:1},
+				{name:34,value:1},
+				{name:36,value:1},
+				{name:37,value:1},
+				{name:39,value:1},
+				{name:40,value:1},
+				{name:41,value:1},
+				{name:42,value:1},
+				{name:43,value:1},
+				{name:44,value:1},
+				{name:45,value:1},
+				{name:48,value:1},
+				{name:52,value:1},
+				{name:53,value:1},
+				{name:55,value:1},
+				{name:57,value:1},
+				{name:60,value:1},
+				{name:61,value:1},
+				{name:62,value:1}
+			]},
+			{name:"Relationship",value:7,
+			children:[
+				{name:38,value:1},
+				{name:8,value:1},
+				{name:12,value:1},
+				{name:13,value:1},
+				{name:49,value:1},
+				{name:52,value:1},
+				{name:24,value:1}
+				]
+			},
+		{name:"Journey",value:10, 
+			children:[
+				{name:2,value:1},
+				{name:5,value:1},
+				{name:6,value:1},
+				{name:56,value:1},
+				{name:24,value:1},
+				{name:25,value:1},
+				{name:26,value:1},
+				{name:59,value:1},
+				{name:28,value:1},
+				{name:61,value:1}
+			]
+		},
+		{name:"Family",value:1,
+			children:[
+				{name:37,value:1}
+			]
+		},
+		{name:"Orientation",value:4,
+			children: [
+				{name:2,value:1},
+				{name:46,value:1},
+				{name:39,value:1},
+				{name:40,value:1}
+			]
+		},
+		{name:"Career",value:5,
+			children:[
+				{name:57,value:1},
+				{name:35,value:1},
+				{name:45,value:1},
+				{name:46,value:1},
+				{name:47,value:1}
+		]
+		},
+		{name:"Politics",value:5,
+			children:
+			[
+				{name:3,value:1},
+				{name:16,value:1},
+				{name:45,value:1},
+				{name:59,value:1},
+				{name:8,value:1}
+			]
+		},
+		{name:"Military",value:2,
+			children:
+			[
+				{name:54,value:1},
+				{name:7,value:1},
+				{name:16,value:1}
+			]
+		}
+	]
+}
+
+function forceRadialThemeClusters(){
+
+	var graph = d3.select('#BieGua>svg').append('g').attr("transform",
+		function(){
+			var result = "translate("+200+","+200+")";
+			return result;
+		}
+	)
+	var numberOfClusters = themes.children.length;
+	console.log(numberOfClusters);
+	// all themes
+
+	var centerX = 200
+	var centerY = 200
+	// radius for the small circle
+	var radius = 6
+	//  radius for the large circle
+	var rad = 100
+	var themesObject = themes.children
+	themesObject.forEach(function(d,i){
+		// the angle of the rotation
+		var angle = Math.PI*2/numberOfClusters*i;
+		var subgraph = graph.append('g');
+		// may not use rotation
+		subgraph.attr("transform",
+			function(){
+				var result = ""
+				// var result = "rotate("+(1*angle)+","+centerX+","+centerY+") ";
+				result+="translate("+(centerX+Math.cos(angle)*rad)+","+(centerY+Math.sin(angle)*rad)+")";
+				return result;
+			}
+		);
+		//transflate to the circle of the graph
+
+		var shift = 7.5
+		// generate the curve and add user interactions
+
+		var len = 200;
+
+		// apply line gnerator on all of the lines
+		console.log("current theme: "+name);
+		var current = d.children; // current object 
+		// console.log(len(current))
+		// console.log(d.children)
+		var lineGraph = subgraph.append('g')
+		current.forEach(
+			(d_line,i_line) =>{
+				var x1 = 0;
+				var y1 = 0;
+				// the x1 and x2 in some cases
+				var num = d_line.name;
+				console.log(d_line)
+				var unshiftHorizontal = -1*Math.cos(angle)*rad;
+				var unshiftVertical = -1* Math.sin(angle)*rad;
+				var shiftHorizontal = len*Math.cos((num-1)/64*2*Math.PI);
+				var shiftVertical = len*Math.sin((num-1)/64*2*Math.PI);
+				var x2 = unshiftHorizontal+shiftHorizontal
+				var y2 = unshiftVertical+shiftVertical
+				// var x = -250+400+(200)*Math.cos((num-1)/64*2*Math.PI);
+				// var y = -250+400+(200)*Math.sin((num-1)/64*2*Math.PI);
+				// x points and y points
+				console.log('')
+				// xs =[x1,x2];
+				// ys =[y1,y2];
+				// console.log([x1,y1])
+				// console.log([x2,y2])
+
+				var xmid = (x1+x2)/2-10
+				var ymid = (y1+y2)/2
+
+				let lineData = [
+					{'x':x1,'y':y1},
+					{'x':xmid,'y':ymid},
+					{'x':x2,'y':y2}
+				];
+				var lineGenerator = d3.line().curve(d3.curveNatural)
+				.x(function(d3){
+					return d3.x;
+				})
+				.y(function(d3){
+					return d3.y;
+				});
+				// draw the line
+				lineGraph.append('path').datum(lineData)
+				.attr('d',lineGenerator)
+				.style('stroke-width',0.1) // the stroke width
+				.style('stroke','white')
+				.style('fill','none')
+				.style('stroke-opacity',0.5); // the color
+				
+		});
+
+		subgraph.append('circle')
+		.attr('r',radius)
+		.attr('cx',
+			function(){
+				// angle sine and consine
+				return 0;
+			}
+		)
+		.attr('cy',
+			function(){
+				// angle m
+				return 0;
+			}
+		).style('fill',
+		function(){
+			return 'grey'
+		});
+
+		// the text label
+		subgraph.append('text')
+		.text(d.name)
+		// .attr('r',radius)
+		.attr('x',
+			function(){
+				// angle sine and consine
+				return -10;
+			}
+		).attr('y',
+			function(){
+				// angle m
+				return -10;
+			}
+		).style('fill',
+		function(){
+			return 'white'
+		})
+		.style('font-size',10)
+		.style('fill-opacity',0.5);
+
+
+
+		subgraph.select('text').on('mouseover',highlightLine).on('mouseout',unhighlightLine);
+
+		subgraph.select('circle').on('mouseover',highlightLine).on('mouseout',unhighlightLine);
+		// subgraph.append('text').
+		// attr('r',radius)
+		// .attr()
+	});
+
+}
+
+
+
+function drawArcs(d3Object,x1,y1,x2,y2,n,k){
+  var cx = (x1+x2)/2;
+  var cy = (y1+y2)/2;
+  var dx = (x2-x1)/2;
+  var dy = (y2-y1)/2;
+  var i;
+  for (i=0; i<n; i++) {
+    if (i==(n-1)/2) {
+      d3Object.line(x1,y1,x2,y2).stroke({width:1}).fill('none');
+    }
+    else {
+      dd = Math.sqrt(dx*dx+dy*dy);
+      ex = cx + dy/dd * k * (i-(n-1)/2);
+      ey = cy - dx/dd * k * (i-(n-1)/2);
+      d3Object.path("M"+x1+" "+y1+"Q"+ex+" "+ey+" "+x2+" "+y2).stroke({width:1}).fill('none');
+    }
+  }
+}
+
+function highlightLine(){
+	var parent = this.parentNode;
+	d3.select(parent).selectAll('path')
+	.style('stroke-width',0.4) // the stroke width
+	.style('stroke','white')
+	.style('stroke-opacity',1.0); // the color
+
+	d3.select(parent).select('circle')
+	.style('stroke-opacity',1.0)
+	.style('stroke','blue')
+	.style('stroke-width',0.6);
+
+	d3.select(parent).select('text').style('font-size',10)
+	.style('font-size',12)
+	.style('fill-opacity',1.0);
+
+}
+function unhighlightLine(){
+	var parent = this.parentNode;
+	d3.select(parent).selectAll('path')
+	.style('stroke-width',0.1) // the stroke width
+	.style('stroke','white')
+	.style('stroke-opacity',0.5); // the color
+
+	d3.select(parent).select('circle')
+	.style('stroke','none')
+	// .style('stroke-opacity',1.0)
+	// .style('stroke','blue')
+	// .style('stroke-width',0.6);
+
+	d3.select(parent).select('text').style('font-size',10)
+	.style('font-size',10)
+	.style('fill-opacity',0.5);
+}
+
+
+
+
+
+// the hiearchical cluster using pack
+function PackStylehemeClusters(){
+	// all themes
+	// var sumReducer = (accum,d)=> {accum+d.count;}
+	// var numberOfClusters = themes.reduce(sumReducer,0);
+	// Add force, 
+	var padding  = 10;
+	// the radius for small circle
+	var circleRadius = 3;
+	// separation between different color circles
+	var clusterPadding = 8;
+	// maximum radius
+	var maxRadius = 12;
+
+	var width = 300
+	var height = 300
+	// Use d3 pack APIs 
+	var pack = d3.pack()
+	.size([width, height])
+	.padding(padding);
+
+	var themesGraph = d3.select('#BieGua>svg').append('g').attr("transform",
+		function(){
+			var result = "translate("+250+","+250+")";
+			return result;
+		}
+	)
+
+	 themes= d3.hierarchy(themes)
+      .sum(function(d) { return d.value; })
+      .sort(function(a, b) { return b.value - a.value; });
+	// var nodes = pack.nodes(themes)
+	var node = themesGraph.selectAll(".node")
+			.data(pack(themes).descendants()).enter()
+		.append("g")
+		.attr('id',function(d){return d.parent? d.parent.data.name+"-"+d.data.name : d.data.name})
+		 	.attr("class", function(d) { return d.children ? "node" : "leaf-node"; })
+			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	// Add user interaction, hover over the themes, add links between gua to theme points
+	node.append("circle")
+  	.attr("r",function(d) { return d.r; })
+  	// .attr("fill", "steel-blue")
+  	.attr("opacity", 0.5)
+  	.attr("stroke", "#ADADAD")
+  	.attr("stroke-width", 2);
+
+  	var texts = node.filter(function(d) {
+  		var check = d.children && d.data.name !== "theme";
+  	 return check }).append("text")
+  	.attr("dx","-0.5em")
+		.attr("dy", "0.2em")
+		.style('fill','white')
+		.text(function(d) { return d.data.name; })
+		.on('mouseover',
+			function(d,i){
+				// for each d connecting the objects
+				// console.log(d)
+				// get the theme name
+				var themeName = this.innerHTML
+				var nodeList = []
+				d.data.children.forEach(
+					(d1,i) =>{
+						nodeList.push(themeName+"-"+d1.name)
+					}
+				)
+				// console.log(nodeList)
+				var lineLink = d3.select('#BieGua>svg').append('g').attr("transform",
+				function(){
+					var result = "translate("+250+","+250+")";
+					return result;
+				}
+			)
+			.attr('class','linksToGua');
+			// var lineGenerator = d3.line()
+				nodeList.forEach(
+					function(d2,i){
+						var translate = d3.select("#BieGua>svg #"+d2).attr('transform');
+						var arr = translate.split(/[:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+						// console.log(arr)
+						var num = d2.split('-')[1]				
+						// console.log(num)
+						var x = -250+400+(200)*Math.cos((num-1)/64*2*Math.PI)
+						var y =-250+400+(200)*Math.sin((num-1)/64*2*Math.PI)
+						var graph = lineLink.append('line')
+						.attr('x1',arr[1])
+						.attr('y1',arr[2])
+						.attr('x2', function(){	
+							return x
+						})
+						.attr('y2',function(){
+							return y
+						})
+						.style('stroke','blue');
+					})
+			}
+
+		).on('mouseout',function(){
+			d3.select('#BieGua>svg').select('.linksToGua').remove()
+		})
+
+		// add user interaction between 
+	
+}
